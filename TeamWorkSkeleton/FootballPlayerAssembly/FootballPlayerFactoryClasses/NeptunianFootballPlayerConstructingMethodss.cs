9fbc53e49447637e3d5d0b7b376d7973c47cea0e
shakuu <todorov.h@gmail.com>
2016-06-17 21:28:11 +0300

    adjust access modifier

--- TeamWorkSkeleton/FootballPlayerAssembly/FootballPlayerFactoryClasses/NeptunianFootballPlayerConstructingMethodss.cs
+++ TeamWorkSkeleton/FootballPlayerAssembly/FootballPlayerFactoryClasses/NeptunianFootballPlayerConstructingMethodss.cs
@@ -5,28 +5,28 @@ namespace FootballPlayerAssembly.FootballPlayerFactoryClasses
 {
     public static partial class FootballPlayerFactory
     {
-        internal static FootballPlayer CreateNeptunianAttacker(FootballPlayer baseStats)
+        private static FootballPlayer CreateNeptunianAttacker(FootballPlayer baseStats)
         {
             var newPlayerName = NeptuneNameGenerator.GenerateName();
             var newPlayer = new RolesClasses.NeptunianFootballPlayer.FootballPlayerFactory.NeptunianAttacker();
             return newPlayer;
         }
 
-        internal static FootballPlayer CreateNeptunianDefender(FootballPlayer baseStats)
+        private static FootballPlayer CreateNeptunianDefender(FootballPlayer baseStats)
         {
             var newPlayerName = NeptuneNameGenerator.GenerateName();
             var newPlayer = new RolesClasses.NeptunianFootballPlayer.FootballPlayerFactory.NeptunianDefender();
             return newPlayer;
         }
 
-        internal static FootballPlayer CreateNeptunianMidfielder(FootballPlayer baseStats)
+        private static FootballPlayer CreateNeptunianMidfielder(FootballPlayer baseStats)
         {
             var newPlayerName = NeptuneNameGenerator.GenerateName();
             var newPlayer = new RolesClasses.NeptunianFootballPlayer.FootballPlayerFactory.NeptunianMidfielder();
             return newPlayer;
         }
 
-        internal static FootballPlayer CreateNeptunianGoalkeeper(FootballPlayer baseStats)
+        private static FootballPlayer CreateNeptunianGoalkeeper(FootballPlayer baseStats)
         {
             var newPlayerName = NeptuneNameGenerator.GenerateName();
             var newPlayer = new RolesClasses.NeptunianFootballPlayer.FootballPlayerFactory.NeptunianGoalkeeper();

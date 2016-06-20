945a20c1ab0d1d8d8badae050bcb405d87d514e5
owolp <zkostov@protonmail.ch>
2016-06-20 17:12:20 +0300

    Update VenusianNameGenerator.cs

--- TeamWorkSkeleton/FootballPlayerAssembly/SpeciesNameGenerators/VenusianNameGenerator.cs
+++ TeamWorkSkeleton/FootballPlayerAssembly/SpeciesNameGenerators/VenusianNameGenerator.cs
@@ -1,11 +1,47 @@
 ﻿namespace FootballPlayerAssembly.SpeciesNameGenerators
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
     internal static class VenusianNameGenerator
     {
+        internal static Dictionary<int, string> FirstPartName = new Dictionary<int, string>()
+        {
+            { 0 , "Mia" },
+            { 1 , "Riu" },
+            { 2 , "Khana" },
+            { 3 , "Pury" },
+            { 4 , "Erikq" },
+            { 5 , "Sey" },
+            { 6 , "Edo" },
+            { 7 , "Livo" },
+            { 8 , "Xipi" },
+            { 9 , "Kai" }
+        };
+
+        internal static Dictionary<int, string> LastPartName = new Dictionary<int, string>()
+        {
+            { 0 , "vin" },
+            { 1 , "margion" },
+            { 2 , "philq" },
+            { 3 , "neuron" },
+            { 4 , "kakud" },
+            { 5 , "novese" },
+            { 6 , "muril" },
+            { 7 , "fyfy" },
+            { 8 , "diansin" },
+            { 9 , "purl" }
+        };
+
         internal static string GenerateName()
         {
-            // TODO: 
-            return "Venusian";
+            var random = new Random();
+            var sb = new StringBuilder();
+            sb.Append(FirstPartName[random.Next(0, 9)]);
+            sb.Append(LastPartName[random.Next(0, 9)]);
+
+            return sb.ToString();
         }
     }
 }

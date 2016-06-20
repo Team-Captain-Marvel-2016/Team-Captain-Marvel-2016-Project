d3345f441ac60ddc15842a17cf35901f8e8f9a84
iliangogov <iliangogov@gmail.com>
2016-06-21 00:41:41 +0300

    Jupiterian.cs

---
+++ TeamWorkSkeleton/FootballPlayerAssembly/SpeciesAbstractClasses/Jupiterian.cs
@@ -0,0 +1,22 @@
+﻿namespace FootballPlayerAssembly.SpeciesAbstractClasses
+{
+    using FootballPlayerAbstractClass;
+    using FootballPlayerFactoryClasses.GenericFootballPlayerClasses;
+
+    internal abstract class Jupiterian:Human
+    {
+        protected Jupiterian(string name) : base(name)
+        {
+        }
+
+        protected Human(string name, int pass, int shoot, int dribble, int save, int tackle, int interception, int awareness, int actionPoints) 
+            : base(name, pass, shoot, dribble, save, tackle, interception, awareness, actionPoints)
+        {
+        }
+
+        internal Human(FootballPlayerFactory.GenericFootballPlayer player) 
+            : base(player)
+        {
+        }
+    }
+}

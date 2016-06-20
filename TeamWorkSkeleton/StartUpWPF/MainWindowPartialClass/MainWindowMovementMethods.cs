e3257459032a0098af57a6203cf366945ae25ef3
shakuu <todorov.h@gmail.com>
2016-06-20 21:58:51 +0300

    Movement Update
    Refactoring WPF project
    UPDATE: Movement with a delegate ( Action )

---
+++ TeamWorkSkeleton/StartUpWPF/MainWindowPartialClass/MainWindowMovementMethods.cs
@@ -0,0 +1,60 @@
+﻿using FootballPlayerAssembly.FootballPlayerAbstractClass;
+
+namespace StartUpWPF
+{
+    using GameStateTrackerAssembly;
+    using PlayingFieldMethodsAssembly;
+    using System;
+    using System.Windows;
+    using VisualizationAssembly.CanvasUtilsClasses;
+
+    public partial class MainWindow : Window
+    {
+        private void MoveUpBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            Move(GameStateTracker.SelectedFootballPlayer.MoveUp);
+        }
+
+        private void MoveDownBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            Move(GameStateTracker.SelectedFootballPlayer.MoveDown);
+        }
+
+        private void MoveLeftBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            Move(GameStateTracker.SelectedFootballPlayer.MoveLeft);
+        }
+
+        private void MoveRightBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            Move(GameStateTracker.SelectedFootballPlayer.MoveRight);
+        }
+
+        private void Move(Action action)
+        {
+            // Move Player On the Grid
+            try
+            {
+                PlayingFieldMethods.UnMarkPlayerPosition
+                    (GameStateTracker.SelectedFootballPlayer);
+
+                action.Invoke();
+
+                PlayingFieldMethods.MarkPlayerPosition
+                    (GameStateTracker.SelectedFootballPlayer);
+            }
+            catch (Exception)
+            {
+                // TODO: Communicate Position was not free
+                PlayingFieldMethods.MarkPlayerPosition
+                    (GameStateTracker.SelectedFootballPlayer);
+
+                return;
+            }
+
+            // Update Canvas Position
+            CanvasChildrenUtilities
+                .UpdateCanvasPosition(PlayFieldCanvas, GameStateTracker.SelectedFootballPlayer);
+        }
+    }
+}

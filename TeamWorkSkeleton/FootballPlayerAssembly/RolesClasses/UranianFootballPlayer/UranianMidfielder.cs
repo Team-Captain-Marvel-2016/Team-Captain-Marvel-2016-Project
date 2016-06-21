340f2f1b2b1e6ed7828bc89454be73d5a12ff419
owolp <zkostov@protonmail.ch>
2016-06-21 10:56:47 +0300

    Create Uranians

---
+++ TeamWorkSkeleton/FootballPlayerAssembly/RolesClasses/UranianFootballPlayer/UranianMidfielder.cs
@@ -0,0 +1,25 @@
+﻿namespace FootballPlayerAssembly.RolesClasses.UranianFootballPlayer
+{
+    using FootballPlayerFactoryClasses.GenericFootballPlayerClasses;
+    using SpeciesAbstractClasses;
+
+    public static partial class FootballPlayerFactory
+    {
+        internal class UranianMidfielder : Venusian
+        {
+            internal UranianMidfielder(FootballPlayerFactoryClasses.GenericFootballPlayerClasses.FootballPlayerFactory.GenericFootballPlayer player) : base(player)
+            {
+            }
+
+            internal UranianMidfielder(string name)
+                : base(name)
+            {
+            }
+
+            internal UranianMidfielder(string name, int pass, int shoot, int dribble, int save, int tackle, int interception, int awareness, int actionPoints) 
+                : base(name, pass, shoot, dribble, save, tackle, interception, awareness, actionPoints)
+            {
+            }
+        }
+    }
+}

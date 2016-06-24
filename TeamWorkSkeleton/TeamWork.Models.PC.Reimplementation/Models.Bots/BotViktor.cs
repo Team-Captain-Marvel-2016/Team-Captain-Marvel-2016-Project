10e4141577f419b521e2be75ddce6830ac1792b0
shakuu <todorov.h@gmail.com>
2016-06-25 00:19:53 +0300

    TrainerBot Update
    BotViktor

---
+++ TeamWorkSkeleton/TeamWork.Models.PC.Reimplementation/Models.Bots/BotViktor.cs
@@ -0,0 +1,16 @@
+﻿namespace TeamWork.Models.PC.Reimplementation.Models.Bots
+{
+    using Contracts;
+    public class BotViktor : PlayerCharacter, IBot
+    {
+        public BotViktor(string name) 
+            : base("BotViktor")
+        {
+        }
+
+        public void DecisionLogic()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

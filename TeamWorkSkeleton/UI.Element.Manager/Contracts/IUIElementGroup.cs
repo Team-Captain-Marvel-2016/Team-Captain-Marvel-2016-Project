230f8f17900acf4eb291eeb2cf3302c00f4e7db4
shakuu <todorov.h@gmail.com>
2016-07-10 11:17:29 +0300

    Update: last commit

--- TeamWorkSkeleton/UI.Element.Manager/Contracts/IUIElementGroup.cs
+++ TeamWorkSkeleton/UI.Element.Manager/Contracts/IUIElementGroup.cs
@@ -1,30 +1,39 @@
 ﻿namespace UI.Element.Manager.Contracts
 {
     using System;
-    using System.Collections;
-    using System.Collections.Generic;
 
     public interface IElementGroup : IComparable<IElementGroup>
     {
         string Name { get; }
+
         int ContainerSize { get; }
 
         void Add(object obj);
+
         void Remove(int index);
 
         void Display();
+
         void Hide();
+
         void Enable();
+
         void Disable();
 
         void SubscribeEnable(ref EventHandler ev);
+
         void SubscribeDisable(ref EventHandler ev);
+
         void SubscribeDisplay(ref EventHandler ev);
+
         void SubscribeHide(ref EventHandler ev);
 
         void UnsubscribeEnable(ref EventHandler ev);
+
         void UnsubscribeDisable(ref EventHandler ev);
+
         void UnsubscribeDisplay(ref EventHandler ev);
+
         void UnsubscribeHide(ref EventHandler ev);
     }
 }
